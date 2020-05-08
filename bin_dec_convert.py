348f1729ea1140959f30d2d5f60372f9492af93e
lenux56 <popelyshelena@gmail.com>
2020-05-08 18:30:28 +0300

    rafactor from pylint

--- bin_dec_convert.py
+++ bin_dec_convert.py
@@ -1,43 +1,43 @@
 '''
 convertation from binary to decimal and from decimal to binary
 '''
-def dec_to_bin(n, res_list):
+def dec_to_bin(dec_number, res_list):
     '''
     Convertation from decimal to binary system
     res_list - list  with binary number
     '''
-    if n < 2:
-        res_list.append(int(n))
+    if dec_number < 2:
+        res_list.append(int(dec_number))
         res_list.reverse()
         return res_list
-    res_list.append(int(n%2))
-    dec_to_bin(n//2, res_list)
+    res_list.append(int(dec_number%2))
+    dec_to_bin(dec_number//2, res_list)
     return ''.join(map(str, res_list))
 
-def bin_to_dec(n):
+def bin_to_dec(bin_number):
     '''
     convert from binary to decimal
     '''
-    return sum((int(x)*2**(len(n)-i-1) for i, x in enumerate(n)))
+    return sum((int(x)*2**(len(bin_number)-i-1) for i, x in enumerate(bin_number)))
 
-def convert(n, system):
+def convert(number, system):
     '''
     Common enter to choose system for convert
     and verify input number
     '''
-    if not isinstance(n, (int, str)):
+    if not isinstance(number, (int, str)):
         print('Error type of number. Accepet str and int')
     if system == 'dec':
-        if not set(str(n)).issubset({'0', '1'}):
-            print(f'Input {n} is not binary number')
+        if not set(str(number)).issubset({'0', '1'}):
+            print(f'Input {number} is not binary number')
         try:
-            print(bin_to_dec(str(n)))
+            print(bin_to_dec(str(number)))
         except:
-            print(f'Programm can`t convert {n} in the {system}')
+            print(f'Programm can`t convert {number} in the {system}')
     elif system == 'bin':
         try:
-            print(dec_to_bin(n, []))
+            print(dec_to_bin(number, []))
         except:
-            print(f'Programm can`t convert {n} in the {system}')
+            print(f'Programm can`t convert {number} in the {system}')
     else:
         print('Error type of system. We convert to the dec or the bin')
--- test_bin_dec_convert.py
+++ test_bin_dec_convert.py
@@ -1,17 +1,26 @@
+'''
+unit test to bin_dec_convert.py
+'''
 import unittest
 import bin_dec_convert
 
 class TestConvert(unittest.TestCase):
-	"""docstring for TestGenPi"unittest.TestCase __init__(self, arg):
-		super(TestGenPi,unittest.TestCase.__init__()
-		self.arg = arg"""
-	def test_dec_to_bin(self):
-			result = bin_dec_convert.dec_to_bin(22, [])
-			self.assertEqual(result, '10110')
-				
-	def test_bin_to_dec(self):
-		result = bin_dec_convert.bin_to_dec('10101')
-		self.assertEqual(result, 21)
+    """docstring for TestGenPi"unittest.TestCase __init__(self, arg):
+    	super(TestGenPi,unittest.TestCase.__init__()
+    	self.arg = arg"""
+    def test_dec_to_bin(self):
+        '''
+        check 22 in binary, waiting output 10110
+        '''
+        result = bin_dec_convert.dec_to_bin(22, [])
+        self.assertEqual(result, '10110')
+
+    def test_bin_to_dec(self):
+        '''
+        check 10101 in decimal, waiting output 21
+        '''
+        result = bin_dec_convert.bin_to_dec('10101')
+        self.assertEqual(result, 21)
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
